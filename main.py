3e7ed12432fca97c236c6f292d1e6ba3241c268f
hj.shin <hj.shin@quintet.co.kr>
2025-08-07 16:37:58 +0900

    feat: validation router 등록

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.routes.test import router as test
+from app.routes.ai_validation import router as validation
 
 app = FastAPI()
 
 app.include_router(test)
+app.include_router(validation)
