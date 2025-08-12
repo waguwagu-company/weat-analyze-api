28f319e100c582f29b82e6a87ba88e1ba6383669
woody35545 <woody35545@gmail.com>
2025-08-12 15:43:02 +0900

    chore: requirements.txt 최신화

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ from fastapi import FastAPI
 from app.routes.pipeline_router import router as pipeline_router
 from app.routes.analysis_router import router as analysis_router
 from app.routes.validation_router import router as validation
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://weat.kro.kr"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(analysis_router)
 app.include_router(validation)
